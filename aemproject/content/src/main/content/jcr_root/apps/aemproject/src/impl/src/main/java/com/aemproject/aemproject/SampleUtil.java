15e93cc57ec42113cca8a523f7ab24659c95eb81
Tagore Sai Vytla <Tagore Sai Vytla>
2016-10-23 20:44:19 -0400

    Template ready

---
+++ aemproject/content/src/main/content/jcr_root/apps/aemproject/src/impl/src/main/java/com/aemproject/aemproject/SampleUtil.java
@@ -0,0 +1,11 @@
+package com.aemproject.aemproject;
+
+/**
+ * Utility class used by script.
+ */
+public class SampleUtil {
+
+    public static String getText() {
+        return "Hello World.";
+    }
+}
